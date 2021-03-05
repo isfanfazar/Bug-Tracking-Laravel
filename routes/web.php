5e515182fec367efcefa14a2985234a4ddb11655
isfanfazar <isfanfazar.ifs@gmail.com>
2021-03-05 22:19:52 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,34 @@ Route::get('/updatebug', [UpdateController::class,'index']);
 Route::get('/deletebug', [DeleteController::class,'index']);
 Route::get('/viewbugs', [ViewController::class,'index']);
 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
