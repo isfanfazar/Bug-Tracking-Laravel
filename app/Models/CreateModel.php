b20c195934fa118108cecde9d91d1bd16eb15f6a
isfanfazar <isfanfazar.ifs@gmail.com>
2021-03-14 21:31:24 +0700

    update

---
+++ app/Models/CreateModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class CreateModel extends Model
+{
+   public function allData()
+   {
+      return DB::table('tbl_view')->get();
+   }
+}
